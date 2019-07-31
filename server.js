4b9268697e66de9b432ad34e8d6def9d8f688049
Gabriel Tan <gabtan99@gmail.com>
2019-07-31 11:49:46 +0800

    organized files to standard directory

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ const urlencoder = bodyparser.urlencoded({
 });
 
 
-app.get
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/home-guest.html")
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/public/registration.html")
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/public/login.html")
+})
+
+
+
 
 
 
