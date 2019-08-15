58ddde2dcf091c771e409d538c926fe8c032b4ef
Gabriel Tan <gabtan99@gmail.com>
2019-08-15 15:09:46 +0800

    take quiz works in search

--- public/js/edit-quiz.js
+++ public/js/edit-quiz.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $("#editform").submit()
     })
 
-    $("button#save-button").click(function(){
+    $("button#save-button").click(function () {
         var id = $("#Form").attr("data-id")
         var title = $("#titlebox").val()
         var subject = $("#subjectbox").val()
@@ -35,12 +35,12 @@ $(document).ready(function () {
     counter = checkNumberOfCards()
     checkCardLabels(counter)
 
-    $('.add-card a').click(function(){
+    $('.add-card a').click(function () {
         createCard()
         return false;
     })
 
-    function createCard(){
+    function createCard() {
         counter++
         const cardElementDiv = document.createElement("div")
         cardElementDiv.className = "card-element"
@@ -57,7 +57,7 @@ $(document).ready(function () {
 
         const cardLabel = document.createElement("div")
         cardLabel.id = "card-label"
-        cardLabel.innerHTML = "Flashcard " + counter 
+        cardLabel.innerHTML = "Flashcard " + counter
 
         const cardsDiv = document.createElement('div')
         cardsDiv.className = "cards"
@@ -83,8 +83,8 @@ $(document).ready(function () {
         const parWhite = document.createElement('p')
         parWhite.innerHTML = "Cards Against Stupidity"
         ///////////////
-        
-        
+
+
 
 
         footerBlack.append(parBlack)
@@ -92,7 +92,7 @@ $(document).ready(function () {
         blackCardDiv.append(textAreaBlack)
         blackCardDiv.append(footerBlack)
 
-        
+
         whiteCardDiv.append(textAreaWhite)
         whiteCardDiv.append(footerWhite)
         cardsDiv.append(blackCardDiv)
@@ -106,8 +106,8 @@ $(document).ready(function () {
         cardElementDiv.append(cardsDiv)
 
         $('#card-container').prepend(cardElementDiv)
-        
-        removeCard.onclick = function(){
+
+        removeCard.onclick = function () {
             console.log("test delete")
             cardElementDiv.remove()
             counter--
@@ -116,17 +116,17 @@ $(document).ready(function () {
         }
     }
 
-    function checkCardLabels(counter){
+    function checkCardLabels(counter) {
         counterTemp = counter
-        $(".card-element").each(function(){
-            $(this).children(".card-header").children("#card-label").html("Flashcard "+counterTemp)
+        $(".card-element").each(function () {
+            $(this).children(".card-header").children("#card-label").html("Flashcard " + counterTemp)
             counterTemp--
         })
     }
 
-    function checkNumberOfCards(){
+    function checkNumberOfCards() {
         counter = 0
-        $(".card-element").each(function(){
+        $(".card-element").each(function () {
             counter++
         })
         return counter
@@ -141,10 +141,10 @@ $(document).ready(function () {
                 "Question": question,
                 "Answer": answer
             }
-    
+
             cards.push(flashCard)
         })
-    
+
         return cards
     }
 
