ea40baf688fd61159a525e7c9268facc5cbedb36
Gabriel Tan <gabtan99@gmail.com>
2019-08-15 16:06:05 +0800

    working na rin edit and take ni my quizzes

---
+++ public/js/quizzes.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $("button.editBtn").click(function () {
+        $("#editid").val($(this).attr("data-id"))
+        $("#editform").submit()
+    })
+
+    $("button.takeBtn").click(function () {
+        $("#takeid").val($(this).attr("data-id"))
+        $("#takeform").submit()
+    })
+
+
+
+
+
+})
