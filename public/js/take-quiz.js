b757554cb4490758a44e92b5a5ed9d1d6c15c002
Gabriel Tan <gabtan99@gmail.com>
2019-08-18 10:37:24 +0800

    added random flashcard order

--- public/js/take-quiz.js
+++ public/js/take-quiz.js
@@ -39,6 +39,8 @@ $(document).ready(function () {
     $('#takeQuizForm').submit(function (e) {
         e.preventDefault()
 
+        shuffle(qdeck)
+
         timerCount = $("#timerCount").val()
         timerMax = timerCount
 
@@ -184,4 +186,22 @@ function guessedAnswer() {
         clearInterval(timer);
         showNextQuestion()
     }
+}
+
+function shuffle(arra1) {
+    var ctr = arra1.length,
+        temp, index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+        ctr--;
+        // And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
 }
